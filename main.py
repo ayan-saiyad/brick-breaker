6ac7b69dd07b4ee24c6296e3471abb8d111a3312
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-06-19 04:09:14 -0500

    Created ball movement
    ball now moves

--- main.py
+++ main.py
@@ -20,10 +20,14 @@ purple = (255, 0, 255)
 
 player_x = 190
 player_direction = 0
-player_speed = 8
+player_speed = 6
 
 ball_x = WIDTH / 2
 ball_y = HEIGHT - 30
+ball_x_direction = 0
+ball_y_direction = 0
+ball_x_speed = 5
+ball_y_speed = 5
 
 board = [[5, 5, 5, 5, 5], [4, 4, 4, 4, 4], [3, 3, 3, 3, 3], [2, 2, 2, 2, 2], [1, 1, 1, 1, 1]]
 
@@ -60,6 +64,8 @@ while run:
             if not active:
                 if event.key == pygame.K_SPACE:
                     active = True
+                    ball_y_direction = -1
+                    bal_x_direction = random.choice([-1, 1])
             else:
                 if event.key == pygame.K_RIGHT:
                     player_direction = 1
@@ -67,11 +73,14 @@ while run:
                     player_direction = -1
 
         if event.type == pygame.KEYUP:
-            if event.type == active:
+
                 if event.key == pygame.K_RIGHT:
                     player_direction = 0
                 if event.key == pygame.K_LEFT:
                     player_direction = 0
+
+    ball_x += ball_x_direction * ball_x_speed
+    ball_y += ball_y_direction * ball_y_speed
 
     player_x += player_direction * player_speed
 
